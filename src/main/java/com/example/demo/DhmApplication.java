9c7212ed56e5002e2e0cbf96c2ac8b9c82ec6613
choihyehyeon <chlgpgus57@naver.com>
2020-01-14 11:23:39 +0900

    fdf

---
+++ src/main/java/com/example/demo/DhmApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DhmApplication.class, args);
+	}
+
+}
