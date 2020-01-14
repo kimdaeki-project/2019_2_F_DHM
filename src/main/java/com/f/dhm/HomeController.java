e39660fefded2af6f13885c213c0272a27c25547
choihyehyeon <chlgpgus57@naver.com>
2020-01-14 11:49:15 +0900

    zzz

---
+++ src/main/java/com/f/dhm/HomeController.java
@@ -0,0 +1,13 @@
+package com.f.dhm;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String index() throws Exception{
+		return "index";
+	}
+}
