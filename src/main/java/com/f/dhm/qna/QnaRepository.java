6049603bdb121c7289e6972e7979926c68a13055
ims330k <ims330k@gmail.com>
2020-01-21 21:41:42 +0900

    c

--- src/main/java/com/f/dhm/qna/QnaRepository.java
+++ src/main/java/com/f/dhm/qna/QnaRepository.java
@@ -1,10 +1,12 @@
 package com.f.dhm.qna;
 
+import java.util.List;
+
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QnaRepository extends JpaRepository<QnaVO, Integer>{
 
 	//public void findByNumOrderByStepAsc()throws Exception;
-	
 	
 }
